4eaeaa6ec57a36be5ea7517896e1d650d2c7ee6f
DavidNail <43179018+DavidNail@users.noreply.github.com>
2018-09-11 12:41:32 -0400

    Add files via upload

---
+++ P2HW1_celciusconverter_DavidNail.py
@@ -0,0 +1,11 @@
+#Convert between Celsius and Fahrenheit
+#9/11/2018
+#CTI-110 P2HW1 - Celsius Fahrenheit Converter
+#David Nail
+celsius=int(input('Enter the temperature in celsius: '))
+f=(celsius*1.8)+32
+print('Termperature in farenheit is: ',f)
+farenheit=int(input('Enter the temperature in farenheit: '))
+c=(farenheit-32)*5/9
+print('Temperature in celsius is ',c)
+
