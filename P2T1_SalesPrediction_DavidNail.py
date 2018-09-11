64065d5463eac284b9e8e86540c67c31c44d1fdb
DavidNail <43179018+DavidNail@users.noreply.github.com>
2018-09-11 12:39:13 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_DavidNail.py
@@ -0,0 +1,7 @@
+# Get the projected total sales
+#9/11/2018
+#CTI-110 P2T1-Sales Prediction
+#David Nail
+total_sales = float(input('Enter the projected sales: '))
+profit = total_sales * 0.23
+print('The profit is $', format(profit, ',.2f'))
